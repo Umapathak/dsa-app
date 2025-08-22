cf547babca8935a2c5b1dd09c27e1094e4d1da09
Umapathak <andromeda21071994@gmail.com>
2025-08-22 10:55:30 +0530

    test.js

---
+++ dsa-app/netlify/functions/test.js
@@ -0,0 +1,7 @@
+// netlify/functions/test.js
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "Test function is working!" })
+  };
+};
